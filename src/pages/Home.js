b0dedb9a21a4e6911fa5830e1c9a035e6430fe96
alex <o.lototskyi@gmail.com>
2023-06-05 10:27:46 +0300

    imperative navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 const HomePage = () => {
+
+    const navigate = useNavigate();
+
+    function navigateHandler() {
+        navigate('/profucts');
+    }
+
     return (
         <>
             <h1>My Home Page</h1>
             <p>
                 Go to <Link to="/products">the list of products</Link>
+            </p>
+            <p>
+                <button onClick={navigateHandler}>Navigate</button>
             </p>
         </>
     );
